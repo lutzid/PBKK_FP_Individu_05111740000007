7a8cdc59a0615664d1da19a3cd06fd7b6acfe62c
lutzid <lut.zidan@gmail.com>
2020-04-28 00:18:57 +0700

    update query sql

--- pbkk_ind.sql
+++ pbkk_ind.sql
@@ -1,13 +1,15 @@
-DROP TABLE IF EXISTS dbo.pemijat
 
+DROP TABLE IF EXISTS dbo.pemijat
+DROP TABLE IF EXISTS dbo.pelanggan
+DROP TABLE IF EXISTS dbo.pemesanan
 CREATE TABLE pemijat (
- id int primary key identity,
+ id varchar (24) primary key,
  nama varchar (200) NOT NULL,
- ktp varchar (17) NOT NULL,
+ ktp varchar (17) unique NOT NULL,
  jenis_kelamin varchar (2) NOT NULL,
  alamat varchar (100) NOT NULL,
- email varchar (50) NOT NULL,
- password varchar (20) NOT NULL,
+ email varchar (200) NOT NULL,
+ password text NOT NULL,
  reset_pass varchar (32) NOT NULL,
  gambar varchar (200) default NULL,
  status varchar (20) NOT NULL,
@@ -16,40 +18,42 @@ CREATE TABLE pemijat (
 )
 
 INSERT INTO pemijat
-(nama, ktp, jenis_kelamin, alamat, email, password, reset_pass, gambar, status, tarif, created_at)
+(id, nama, ktp, jenis_kelamin, alamat, email, password, reset_pass, gambar, status, tarif, created_at)
 VALUES
-('Pemijat1', '0000000000000000', 'L', 'Surabaya', 'pemijat@gmail.com', 'password', 'null', 'gambar', 1, 50000, '2020-04-21 22:18:44'),
-('Pemijat2', '0000000000000000', 'L', 'Surabaya', 'pemijat@gmail.com', 'password', 'null', 'gambar', 1, 50000, '2020-04-21 22:18:44')
-
-
-DROP TABLE IF EXISTS dbo.pelanggan
+('asdasdasdsadsa', 'Pemijat1', '0000000000000000', 'L', 'Surabaya', 'pemijat@gmail.com', 'password', 'null', 'gambar', 'Tidak Aktif', 50000, '2020-04-21 22:18:44'),
+('eqwewqdasdasdd', 'Pemijat2', '0000000000000001', 'L', 'Surabaya', 'pemijat@gmail.com', 'password', 'null', 'gambar', 'Aktif', 50000, '2020-04-21 22:18:44')
 
 CREATE TABLE pelanggan (
- id int primary key identity,
+ id varchar (24) primary key,
  nama varchar (200) NOT NULL,
- ktp varchar (17) NOT NULL,
+ ktp varchar (17) unique NOT NULL,
  jenis_kelamin varchar (2) NOT NULL,
  alamat varchar (100) NOT NULL,
- email varchar (50) NOT NULL,
- password varchar (20) NOT NULL,
+ email varchar (200) NOT NULL,
+ password text NOT NULL,
  reset_pass varchar (32) NOT NULL,
  gambar varchar (200) default NULL,
  created_at datetime default null
 )
 
 INSERT INTO pelanggan
-(nama, ktp, jenis_kelamin, alamat, email, password, reset_pass, gambar, created_at)
-VALUES
-('Pemijat1', '0000000000000000', 'L', 'Surabaya', 'pemijat@gmail.com', 'password', 'null', 'gambar', '2020-04-21 22:18:44')
+(id, nama, ktp, jenis_kelamin, alamat, email, password, reset_pass, gambar, created_at)
+VALUES 
+('easdqweqdasdasdd', 'Pelanggan1', '0000000000000000', 'L', 'Surabaya', 'pemijat@gmail.com', '$2y$12$M0hCaXN3Tlh0dmE5cWZCe.tVP/FtBGE/92.ZUs08lo7PBBy.aLy7m', 'null', 'gambar', '2020-04-21 22:18:44'),
+('wi6BMroM11Di2nfIaR8zA1A5', 'Pelanggan', '321313', 'L', 'Surabaya', 'plg23@test.com', '$2y$12$M0hCaXN3Tlh0dmE5cWZCe.tVP/FtBGE/92.ZUs08lo7PBBy.aLy7m', 'null', NULL, null)
+OPTION (QUERYTRACEON 460);
+
+-- ('eqwewqdasdasdd', 'Pelanggan1', '0000000000000000', 'L', 'Surabaya', 'pemijat@gmail.com', '$2y$12$M0hCaXN3Tlh0dmE5cWZCe.tVP/FtBGE/92.ZUs08lo7PBBy.aLy7m', 'null', 'gambar', '2020-04-21 22:18:44'),
 
 CREATE TABLE pemesanan (
- id int primary key identity,
- pemijat_id int NOT NULL,
- pelanggan_id int NOT NULL,
- status varchar (20) NOT NULL
+ id varchar (24) primary key,
+ pemijat_id varchar (24) NOT NULL,
+ pelanggan_id varchar (24) NOT NULL,
+ status varchar (20) NOT NULL,
+ created_at datetime default null
 )
 
 INSERT INTO pemesanan
-(pemijat_id, pelanggan_id, status)
+(id, pemijat_id, pelanggan_id, status)
 VALUES
-(1, 1, 'Pending')
+('sadqwnasldlasdn', 'eqwewqdasdasdd', 'eqwewqdasdasdd', 'Pending')
