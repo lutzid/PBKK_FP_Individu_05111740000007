e8ebdbe9543d85668e82342849082939b8b79c15
lutzid <lut.zidan@gmail.com>
2020-04-28 00:13:26 +0700

    update loader

--- apps/config/loader.php
+++ apps/config/loader.php
@@ -14,6 +14,7 @@ $loader->registerNamespaces(array(
 	'Phalcon\Db\Adapter\Pdo' => APP_PATH . '/lib/Phalcon/Db/Adapter/Pdo',
 	'Phalcon\Db\Dialect' => APP_PATH . '/lib/Phalcon/Db/Dialect',
 	'Phalcon\Db\Result' => APP_PATH . '/lib/Phalcon/Db/Result',
+	'MyModel' => APP_PATH . '/modules/dashboard/Core/Domain/Model'
 ));
 
 $loader->register();
