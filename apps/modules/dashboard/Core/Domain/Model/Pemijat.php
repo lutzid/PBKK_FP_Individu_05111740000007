b453c2865c334fed6cd4689f4e6afe394c921af0
lutzid <lut.zidan@gmail.com>
2020-04-28 00:15:20 +0700

    add registration method and hasmany relation

--- apps/modules/dashboard/Core/Domain/Model/Pemijat.php
+++ apps/modules/dashboard/Core/Domain/Model/Pemijat.php
@@ -5,7 +5,8 @@ namespace MyModel;
 use Phalcon\Mvc\Model;
 use MyModel\My_Model;
 
-class Pemijat extends Model {
+class Pemijat extends My_Model
+{
     public $id;
     public $nama;
     public $ktp;
@@ -20,5 +21,33 @@ class Pemijat extends Model {
 
     public function initialize () {
         $this->setSource('pemijat');
+
+        $this->hasMany(
+            'id',
+            'MyModel\Pemesanan',
+            'pemijat_id',
+            [
+                'alias' => 'pemesanan'
+            ]
+        );
+    }
+
+    public function registrasi($data)
+    {
+        $this->id = $data['id'];
+        $this->nama = $data['nama'];
+        $this->ktp = $data['ktp'];
+        $this->jenis_kelamin = $data['jenis_kelamin'];
+        $this->alamat = $data['alamat'];
+        $this->email = $data['email'];
+        $this->password = $data['password'];
+        $this->reset_pass = $data['reset_pass'];
+        $this->gambar = $data['gambar'];
+        $this->status = $data['status'];
+        $this->tarif = $data['tarif'];
+    }
+
+    public function getTarif(){
+        return $this->tarif;
     }
 }
