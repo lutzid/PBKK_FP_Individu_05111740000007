70410ac625d88a116448193258de089e3cfcaf14
lutzid <lut.zidan@gmail.com>
2020-04-28 00:13:47 +0700

    update session and response di

--- apps/config/services.php
+++ apps/config/services.php
@@ -1,6 +1,7 @@
 <?php
 
-use Phalcon\Session\Adapter\Files as Session;
+use Phalcon\Session\Manager as SessionManager;
+use Phalcon\Session\Adapter\Stream as Session;
 use Phalcon\Security;
 use Phalcon\Mvc\Dispatcher;
 use Phalcon\Events\Event;
@@ -16,15 +17,25 @@ $container['config'] = function() use ($config) {
 };
 
 $container->setShared('session', function() {
-    $session = new Session();
-	$session->start();
+    // $session = new Session();
+	// $session->start();
+
+    $session = new SessionManager();
+    $files = new Session(
+        [
+            'savePath' => '/tmp',
+        ]
+    );
+    $session
+        ->setAdapter($files)
+        ->start();
 
 	return $session;
 });
 
 $container->setShared('response', function () {
     $response = new \Phalcon\Http\Response();
-    $response->setContentType('application/json', 'utf-8');
+    $response->setContentType('text/html', 'utf-8');
 
     return $response;
 });
