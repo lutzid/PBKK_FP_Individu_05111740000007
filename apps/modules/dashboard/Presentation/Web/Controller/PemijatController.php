868e75c38741d69d7b3bcdbfa2238a323e7b23cf
lutzid <lut.zidan@gmail.com>
2020-04-28 00:17:10 +0700

    add pemijatcontroller and it's usecase

---
+++ apps/modules/dashboard/Presentation/Web/Controller/PemijatController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace MyModule\Dashboard\Presentation\Web\Controller;
+
+use Phalcon\Mvc\Controller;
+use MyModule\Dashboard\Presentation\Web\Controller\BaseController;
+use Phalcon\Http\Request;
+use MyModel\Pemijat;
+
+class PemijatController extends BaseController 
+{
+    public function registerIndexAction()
+    {
+        // $db = $this->getDI()->get('db');
+
+        // $sql = "SELECT nama, alamat, gambar from pemijat";
+
+        // $result = $db->fetchAll($sql, \Phalcon\Db\Enum::FETCH_ASSOC);
+
+        // echo json_encode($result);
+        // var_dump($result); die;
+        // echo "register pemijat";
+        // $this->view->pick('views/authentication/register');
+    }
+
+    public function registerAction()
+    {
+        if($this->request->isPost())
+        {
+            $random = new \Phalcon\Security\Random();
+            $data = $_POST;
+            $data['id'] = $random->base64(18);
+            $data['password'] = $this->security->hash($data['password']);
+            $data['reset_pass'] = 'null';
+            $data['gambar'] = 'storage/avatar3.jpg';
+            $data['status'] = 'Tidak Aktif';
+            $data['tarif'] = 0;
+            $new_pemijat = new Pemijat();
+            $new_pemijat->registrasi($data);
+            // var_dump($new_pemijat);die;
+            $new_pemijat->save();
+           return $this->response->redirect('login/pemijat')->send();
+        }
+    }
+
+    public function loginIndexAction()
+    {
+
+    }
+
+    public function loginAction()
+    {
+        $email    = $this->request->getPost('email');
+        $pass   = $this->request->getPost('password');
+    	$get_pemijat   = Pemijat::findFirst(
+            [
+                "conditions" => "email = :email:",
+                "bind" => [
+                    "email"   => $email
+                ],
+            ]
+        );
+        if($get_pemijat === false){
+            $this->flashSession->error("Email atau Password yang anda inputkan salah.");
+            return $this->response->redirect('login');
+        }else{
+            if(!$this->security->checkHash($pass,$get_pemijat->password)){
+                $this->flashSession->error("Email atau Password yang anda inputkan salah.");
+                return $this->response->redirect('login');
+            }
+            // $this->session->set('auth',[
+            //     'username' => $user->username
+            // ]);
+            $this->session->set('auth', $get_pemijat);
+            $this->session->set('role', 'Pemijat');
+        }
+
+        return $this->response->redirect('/home');
+    }
+
+    public function logoutAction()
+    {
+        $this->session->destroy();
+        return $this->response->redirect('');
+    }
+
+    public function editAction()
+    {
+        if($this->request->isPost())
+        {
+            $id = $this->getSessionId();
+            $user = Pemijat::findFirst(
+                [
+                    "conditions" => "id = :id:",
+                    "bind" => [
+                        "id"   => $id
+                    ],
+                ]
+            );
+            // $user->status = 'Aktif';
+            $data = $_POST;
+            $data['id'] = $id;
+            $data['reset_pass'] = 'null';
+            $user->registrasi($data);
+            // $user->save();
+            if($this->request->hasFiles() == true){
+                $uploads = $this->request->getUploadedFiles();
+                // var_dump($this->request->getUploadedFiles());die();  
+                $isuploaded = false;
+                $allpath = "";
+                foreach($uploads as $up)
+                {
+                    $path = 'storage/'.time().'-'.strtolower($up->getname());
+                    $fpath = BASE_PATH . "/public/" . $path;
+                    if($up->moveTo($fpath)){
+                        $isUploaded = true;
+                        $allpath.=$path;
+                    }else{
+                        $isUploaded = false;
+                    }
+
+                }
+                $user->gambar = $allpath;
+            }
+            // var_dump($user);die;
+            if($user->save())
+            {
+                return $this->response->redirect('profile');
+            }
+        }
+    }
+}
